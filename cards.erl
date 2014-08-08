36090ddd92a1afed8302c47a90680f636c260414
arunsk <arunsk.tec@gmail.com>
2014-08-07 21:19:54 -0400

    Étude 7-6: Explaining an Algorithm

--- cards.erl
+++ cards.erl
@@ -1,5 +1,5 @@
 -module(cards).
--export([make_deck/0, show_deck/1]).
+-export([make_deck/0, show_deck/1, shuffle/1]).
 
 make_deck() ->
     [{Value, Suit} || Value <- ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
@@ -7,3 +7,11 @@ make_deck() ->
 
 show_deck(Deck) ->
     lists:foreach(fun(Item) -> io:format("~p~n", [Item]) end, Deck).
+
+shuffle(List) -> shuffle(List, []).
+
+shuffle([], Acc) -> Acc;
+
+shuffle(List, Acc) ->
+    {Leading, [H|T]} = lists:split(random:uniform(length(List)) - 1, List),
+    shuffle(Leading ++ T, [H|Acc]).
