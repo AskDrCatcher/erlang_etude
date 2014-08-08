5bf9fc635db2a4e53182b076122da993e98a3fbb
arunsk <arunsk.tec@gmail.com>
2014-08-07 21:05:03 -0400

    Étude 7-4: Using lists:split/2

--- dates.erl
+++ dates.erl
@@ -9,23 +9,33 @@ date_parts(DateStr) ->
      element(1, string:to_integer(MStr)),
      element(1, string:to_integer(DStr))].
 
-julian (ISODate) ->
-    [Year, Month, Day] = date_parts(ISODate),
-    DaysInFeb = 
-        case is_leap_year(Year) of
-            true  -> 29;
-            false -> 28
-        end,
-    DaysPerMonth = [31, DaysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    julian (Year, Month, Day, DaysPerMonth, 0).
+%julian (ISODate) ->
+%    [Year, Month, Day] = date_parts(ISODate),
+%    DaysInFeb = 
+%        case is_leap_year(Year) of
+%            true  -> 29;
+%            false -> 28
+%        end,
+%    DaysPerMonth = [31, DaysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+%    julian (Year, Month, Day, DaysPerMonth, 0).
 
-julian (Year, Month, Day, DaysPerMonth, A) ->
-    [H | T] = DaysPerMonth,
-    DaysPerMonth_len = length(DaysPerMonth),
-    %io:format("here in true ~p , ~p~n", [DaysPerMonth_len, Month]),
-    case Month > (13 - DaysPerMonth_len) of
-        true  -> julian (Year, Month, Day, T, A + H);
-        false   -> A + Day
+%julian (Year, Month, Day, DaysPerMonth, A) ->
+%    [H | T] = DaysPerMonth,
+%    DaysPerMonth_len = length(DaysPerMonth),
+%    %io:format("here in true ~p , ~p~n", [DaysPerMonth_len, Month]),
+%    case Month > (13 - DaysPerMonth_len) of
+%        true  -> julian (Year, Month, Day, T, A + H);
+%        false   -> A + Day
+%    end.
+
+julian(DateStr) ->
+    DaysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    [Y, M, D] = date_parts(DateStr),
+    {Sublist, _} = lists:split(M - 1, DaysPerMonth),
+    Total = lists:foldl(fun(V, A) -> V + A end, 0, Sublist),
+    case M > 2 andalso is_leap_year(Y) of
+        true  -> Total + D + 1;
+        false -> Total + D
     end.
 
 is_leap_year(Year) ->
